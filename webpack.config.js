0dd0380a798cc256a488de17a29c1d1af13d693d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-21 09:22:12 -0500

    chore(webpack) - Roughed out 'webpack' config. file. Updated project structure.

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: './src/index',
+    output: {
+        filename: 'bundle.js',
+        path: './dist/',
+        publicPath: './'
+    },
+    module: {},
+    plugins: []
+}
